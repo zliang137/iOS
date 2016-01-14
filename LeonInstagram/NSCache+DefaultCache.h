a44b4721efe60fee3c494337d05908101b906cb0
Zhao Liang <Leon@ZhaodeMacBook-Pro.local>
2016-01-14 09:46:07 -0500

    fix bugs

--- LeonInstagram/NSCache+DefaultCache.h
+++ LeonInstagram/NSCache+DefaultCache.h
@@ -2,7 +2,7 @@
 //  NSCache+DefaultCache.h
 //  LeonInstagram
 //
-//  Created by Weiyu Huang on 1/13/16.
+//  Created by Leon Liang on 1/13/16.
 //  Copyright © 2016 Leon. All rights reserved.
 //
 
